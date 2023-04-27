8e9bb9fa31a3a3d531d20941bf52078e79b4d9aa
ChedvihasPunnam <cpunnam@asu.edu>
2023-04-27 14:53:48 -0700

    Updated revLexer

--- src/revLexer.py
+++ src/revLexer.py
@@ -1,9 +1,20 @@
 import sys
+from functools import reduce
+from tokenize import tokenize, untokenize, NUMBER, STRING, NAME, OP
+import re
+import io
+import tokenize
+
 def revlexer(input_file):
     #checking file extension
     extension = input_file[-4:]
-    if extension != '.rev':
+    if extension != 'GIAA':
         raise ValueError("Given file extension is not supported!")
+    tokens_rev_list = []
+    output = ''
+    output = output + ' ['
+    code = open(input_file, 'r').read()
+    tokens_rev_value = tokenize.tokenize(io.BytesIO(code.encode('utf-8')).readline)
 
 if __name__ == "__main__":
     input_file = sys.argv[1]    
